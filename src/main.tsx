1b813d1a5aa58f034bc2eb39b7273fbbcd97c140
Yunseok <watemark1017@gmail.com>
2024-09-25 09:06:29 +0900

    [FEAT] - 파일 구조 라우팅 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "app/app";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
