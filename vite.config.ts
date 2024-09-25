8c18d04059a01ac2d05c6f3a70128e72c8dffcd8
Yunseok <watemark1017@gmail.com>
2024-09-25 18:57:02 +0900

    [CHORE] - 패키지 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import reactRouterPlugin from 'vite-plugin-next-react-router';
+import withReactRouter from 'vite-plugin-next-react-router';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
-    reactRouterPlugin({
+    withReactRouter({
       pageDir: 'src/pages',
       extensions: ['js', 'jsx', 'ts', 'tsx'],
-      // like '_app' in Next.js, `_document` is not supported since all rendering is done in client side
       layout: '_layout',
     }),
   ],
