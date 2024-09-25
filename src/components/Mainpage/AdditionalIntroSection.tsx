7bcda22179e846b1767bc2c07b866bd6d0920afd
liupei8979 <mintaekim@stamper.network>
2024-09-26 02:41:38 +0900

    [FEAT] - Text 수정

--- src/components/Mainpage/AdditionalIntroSection.tsx
+++ src/components/Mainpage/AdditionalIntroSection.tsx
@@ -18,15 +18,15 @@ export default function AdditionalIntroSection() {
 
     const additionalItems = [
         {
-            text: '추가 기능 1에 대한 설명',
+            text: '과연 구름 다리는',
             backgroundImage: '/path/to/image4.jpg',
         },
         {
-            text: '추가 기능 2에 대한 설명',
+            text: '무엇이 그렇게도',
             backgroundImage: '/path/to/image5.jpg',
         },
         {
-            text: '추가 기능 3에 대한 설명',
+            text: '다를까요',
             backgroundImage: '/path/to/image6.jpg',
         },
     ];
@@ -53,8 +53,8 @@ export default function AdditionalIntroSection() {
             {/* 두 번째 박스 그룹 */}
             <div className="w-full text-left mt-16">
                 <h2 className="text-3xl font-bold mb-4 pb-10">
-                    구름다리의 <br />
-                    추가 기능은 무엇일까요?
+                    구름다리 함께라면, <br />
+                    무엇이 다른가요?
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
                     {additionalItems.map((item, index) => (
