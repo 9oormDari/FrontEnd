d2f7de5d598c5d57c68ec725c44fe3b87e91d13c
Yunseok <watemark1017@gmail.com>
2024-09-25 22:35:41 +0900

    [FIX] - 테일윈드가 적용되지 않던 문제 수정
    - postcss.config.js가 누락되어 있었음
    - tailwind.config.js에서 tsx 파일이 빠져있었음

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 필요한 파일 경로를 설정하세요.
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
