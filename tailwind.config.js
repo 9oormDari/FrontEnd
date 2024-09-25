1d26561be52846f6dbab4f0dcb23a23159570cd7
Yunseok <watemark1017@gmail.com>
2024-09-26 00:00:05 +0900

    [FEAT] - Pretendard 폰트 추가
    - 와이어프레임 피그마의 폰트를 기본 폰트가 되도록 설정했습니다.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pretendard: [
+          '"Pretendard Variable"', 
+          'Pretendard', 
+          '-apple-system', 
+          'BlinkMacSystemFont', 
+          'system-ui', 
+          'Roboto', 
+          '"Helvetica Neue"', 
+          '"Segoe UI"', 
+          '"Apple SD Gothic Neo"', 
+          '"Noto Sans KR"', 
+          '"Malgun Gothic"', 
+          '"Apple Color Emoji"', 
+          '"Segoe UI Emoji"', 
+          '"Segoe UI Symbol"', 
+          'sans-serif'
+        ],
+      },
+    },
   },
   plugins: [],
 }
