2482fc07033b61574e26b771b7e706e98d350886
Yunseok <watemark1017@gmail.com>
2024-09-26 00:22:17 +0900

    [FEAT] - 헤더 생성, 링크 추가

---
+++ src/components/Header/NavLink.tsx
@@ -0,0 +1,24 @@
+import { useLocation } from 'react-router-dom';
+import cn from '../../lib/cn.ts';
+
+interface NavLinkProps {
+  href: string;
+  label: string;
+}
+
+export default function NavLink({ href, label }: NavLinkProps) {
+  const location = useLocation();
+  const isActive = location.pathname === href;
+
+  return (
+    <a
+      href={href}
+      className={cn(
+        "text-2xl transition p-4", 
+        `${isActive ? 'text-black font-bold' : 'text-gray-400 hover:text-gray-600'}`
+      )}
+    >
+      {label}
+    </a>
+  );
+}
