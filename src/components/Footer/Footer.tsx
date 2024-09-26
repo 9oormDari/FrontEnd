ebf2fd7d132ab33cd08a2e2a2390d32c6ae8d45b
Yunseok <watemark1017@gmail.com>
2024-09-26 22:43:54 +0900

    [FEAT] - 모바일에서 푸터 조정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,43 +1,49 @@
+import cn from '../../lib/cn';
+
 export default function Footer() {
     return (
-        <footer className="bg-[#47484A] h-[150px] flex items-center justify-between px-20">
+        <footer className={cn(
+            "bg-[#47484A] py-5 md:py-8 flex flex-col md:flex-row items-center",
+            "justify-between px-5 md:px-20"
+            )}
+        >
             {/* 로고 영역 */}
             <img
                 src="/logo_white.svg"
-                alt="logo_white"
-                className="w-[216px] h-[80px]"
+                alt="회사 로고 (흰색)"
+                className="w-[108px] h-[40px] md:w-[216px] md:h-[80px] mb-4 md:mb-0"
             />
 
             {/* 역할 및 이름 리스트 영역 */}
-            <div className="flex flex-wrap text-white text-sm justify-center space-x-16">
-                {/* 왼쪽 그룹 (기획 + 디자인) */}
-                <div className="flex flex-col items-center">
+            <div className="text-white text-xs md:text-sm flex flex-col space-y-4">
+                {/* 첫 번째 줄 (기획 + 프론트) */}
+                <div className="flex flex-wrap justify-end space-x-4">
                     {/* 기획 */}
-                    <div className="flex flex-row items-center space-x-2">
-                        <span className="font-bold p-4">기획</span>
+                    <div className="flex items-center space-x-2 ">
+                        <span className="font-bold p-1 md:p-2">기획</span>
                         <span>김규리</span>
                     </div>
 
-                    {/* 디자인 */}
-                    <div className="flex flex-row items-center space-x-2">
-                        <span className="font-bold p-4">디자인</span>
-                        <span>김보경</span>
-                    </div>
-                </div>
-
-                {/* 오른쪽 그룹 (프론트 + 백엔드) */}
-                <div className="flex flex-col items-center">
                     {/* 프론트 */}
-                    <div className="flex flex-row items-center space-x-2">
-                        <span className="font-bold p-4">프론트</span>
+                    <div className="flex items-center space-x-2 ">
+                        <span className="font-bold p-1 md:p-2">프론트</span>
                         <span>김민태</span>
                         <span>송윤석</span>
                         <span>최세연</span>
                     </div>
+                </div>
+
+                {/* 두 번째 줄 (디자인 + 백엔드) */}
+                <div className="flex flex-wrap justify-end space-x-4">
+                    {/* 디자인 */}
+                    <div className="flex items-center space-x-2 ">
+                        <span className="font-bold p-1 md:p-2">디자인</span>
+                        <span>김보경</span>
+                    </div>
 
                     {/* 백엔드 */}
-                    <div className="flex flex-row items-center space-x-2">
-                        <span className="font-bold p-4">백엔드</span>
+                    <div className="flex items-center space-x-2 ">
+                        <span className="font-bold p-1 md:p-2">백엔드</span>
                         <span>이재혁</span>
                         <span>이현서</span>
                         <span>한현규</span>
