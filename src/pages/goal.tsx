0feb50779dbb41acef54056065523527d4ff14f8
Yunseok <watemark1017@gmail.com>
2024-09-28 00:01:41 +0900

    [FEAT] - 목표 점검 페이지에 구름스크린 렌더링

--- src/pages/goal.tsx
+++ src/pages/goal.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import NeedLoginComponents from '../components/NeedLoginComponents.tsx';
+import GoormScreen from '../components/Goalpage/GoormScreen.tsx';
 import cn from '../lib/cn.ts';
 
 export default function Goal() {
@@ -14,16 +15,15 @@ export default function Goal() {
     return (
         <div
             className={cn(
-                'flex flex-col items-center justify-start min-h-screen',
-                'pb-[50px] pt-[50px] gap-[50px]'
+                'flex flex-col items-center justify-start w-full',
+                'pb-[50px] gap-[50px] min-h-screen'
             )}
         >
         {/* 로그인 확인 컴포넌트 */}
         { !isLogin ? <NeedLoginComponents /> : 
         <>
-            <div className="flex flex-col items-center justify-center">
-                <h1 className="text-2xl md:text-4xl font-bold mt-8">목표 페이지</h1>
-                <h1 className="text-2xl md:text-4xl font-bold mt-2">구현 중입니다</h1>
+            <div className="flex flex-col items-center justify-center w-full ">
+                <GoormScreen />
             </div>
         </>}
         </div>
