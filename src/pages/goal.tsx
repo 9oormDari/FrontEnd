8be9a8ea8090ccce1ef16981018bd41d77efe068
Yunseok <watemark1017@gmail.com>
2024-09-27 21:15:50 +0900

    [FEAT] - 목표점검 페이지 생성 및 로그인 검사 추가

--- src/pages/goal.tsx
+++ src/pages/goal.tsx
@@ -1,10 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import NeedLoginComponents from '../components/NeedLoginComponents.tsx';
-import Goals from '../components/Mypage/Goals';
-import Profile from '../components/Mypage/Profile';
 import cn from '../lib/cn.ts';
 
-export default function MyPage() {
+export default function Goal() {
     const [isLogin, setIsLogin] = useState(false);
     
     useEffect(() => {
@@ -20,12 +18,14 @@ export default function MyPage() {
                 'pb-[50px] pt-[50px] gap-[50px]'
             )}
         >
-            {/* 로그인 확인 컴포넌트 */}
-            { !isLogin ? <NeedLoginComponents /> : 
-            <>
-                <Profile /> 
-                <Goals />
-            </>}
+        {/* 로그인 확인 컴포넌트 */}
+        { !isLogin ? <NeedLoginComponents /> : 
+        <>
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="text-2xl md:text-4xl font-bold mt-8">목표 페이지</h1>
+                <h1 className="text-2xl md:text-4xl font-bold mt-2">구현 중입니다</h1>
+            </div>
+        </>}
         </div>
     );
 }
