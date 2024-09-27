fa5a166dbb6e7ee7886cd47fdec4c95abb8397c7
Yunseok <watemark1017@gmail.com>
2024-09-27 18:04:38 +0900

    [STYLE] - 로그인 리다렉창 모바일 신경쓰기

--- src/components/NeedLoginComponents.tsx
+++ src/components/NeedLoginComponents.tsx
@@ -4,14 +4,14 @@ import cn from '../lib/cn.ts';
 export default function NeedLoginComponents() {
     return (
         <div className="flex flex-col items-center justify-center">
-            <img src={NotGood} alt="Not Good"/>
-            <h1 className="text-4xl font-bold mt-8">로그인 후</h1>
-            <h1 className="text-4xl font-bold mt-2">이용할 수 있는 기능이에요</h1>
+            <img src={NotGood} alt="Not Good" className='w-1/2 h-1/2'/>
+            <h1 className="text-2xl md:text-4xl font-bold mt-8">로그인 후</h1>
+            <h1 className="text-2xl md:text-4xl font-bold mt-2">이용할 수 있는 기능이에요</h1>
             <a href="/login">
                 <button 
                     className={cn(
-                      "mt-10 px-40 py-4 bg-blue-500 rounded-lg",
-                      "text-white text-2xl font-bold"
+                      "mt-5 md:mt-10 px-20 md:px-40 py-4 bg-blue-500 rounded-lg",
+                      "text-white text-base md:text-2xl font-bold"
                     )}
                 >
                   로그인하러 가기
