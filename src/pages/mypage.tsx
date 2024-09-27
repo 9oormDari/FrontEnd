29c21ad4d6cc03d46c62abc3b51d40723f6cf13d
Yunseok <watemark1017@gmail.com>
2024-09-27 14:52:58 +0900

    [FEAT] - 일단 임시로 mypage에 로그인 요구창 임포트

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+import NeedLoginComponents from '../components/NeedLoginComponents.tsx';
 import Goals from '../components/Mypage/Goals';
 import Profile from '../components/Mypage/Profile';
 import cn from '../lib/cn.ts';
 
 export default function MyPage() {
+    const [isLogin, setIsLogin] = useState(false);
+
     return (
         <div
             className={cn(
@@ -10,6 +14,11 @@ export default function MyPage() {
                 'pb-[50px] pt-[50px] gap-[50px]'
             )}
         >
+            {/* 
+                * 로그인 필요 컴포넌트 
+                * 로그인 확인 방법을 모르겠어서 일단 임포트 했습니다.
+            */}
+            { isLogin ? null : <NeedLoginComponents /> }
             {/* 프로필 영역 */}
             <Profile />
             {/* 목표 영역 */}
