e1c741f75eef805e5d82ea09bb996bf405bd2e15
Yunseok <watemark1017@gmail.com>
2024-09-27 23:28:50 +0900

    [FEAT] - 구름들 크기 조정 및 칼라 구름에 디스크 추가

--- src/components/Goalpage/GoormScreen/CloudComponent.tsx
+++ src/components/Goalpage/GoormScreen/CloudComponent.tsx
@@ -1,4 +1,6 @@
 import cn from '../../../lib/cn';
+import CloudDisk from '../../../assets/GoalPage/CloudDisk.svg';
+import ColorCloud from '../../../assets/GoalPage/ColorCloud.svg';
 
 interface CloudComponentProps {
     cloudType: string;
@@ -13,11 +15,26 @@ export default function CloudComponent({ cloudType, colStart, rowStart, hidden }
             className={cn(
                 `col-start-${colStart}`, 
                 `row-start-${rowStart}`, 
-                "flex justify-center",
+                "flex justify-center flex-col items-center",
                 `${hidden ? "hidden" : ""}`
             )}
         >
-            <img src={cloudType} alt="구름" className="w-16 h-16 md:w-32 md:h-32" />
+            <img 
+                src={cloudType}
+                alt="구름" 
+                className="md:w-[220px] md:h-[220px]"
+                style={{zIndex: 1}}
+            />
+            <img 
+                src={CloudDisk} 
+                alt="받침" 
+                className={cn(
+                    "w-1/4 h-1/4 md:w-[288px] md:h-[156px]",
+                    "absolute mt-20 md:mt-[200px]", 
+                    `${cloudType === ColorCloud ? "" : "hidden"}`
+                )}
+                style={{zIndex: 0}}
+            />
         </div>
     );
 }
