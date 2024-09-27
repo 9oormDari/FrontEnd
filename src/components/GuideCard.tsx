39d9b37a8362cb11e4955159297aae7c612bc41a
liupei8979 <mintaekim@stamper.network>
2024-09-27 16:06:59 +0900

    [FEAT] - guide pc 버전 적용

--- src/components/GuideCard.tsx
+++ src/components/GuideCard.tsx
@@ -14,12 +14,13 @@ const GuideCard: React.FC<GuideCardProps> = ({
     return (
         <div
             className={cn(
-                'max-w-sm rounded-lg overflow-hidden shadow-lg bg-[#E9EBF8] border border-gray-200',
-                'w-[234px] h-[300px] m-4'
+                'max-w-sm rounded-[20px] overflow-hidden shadow-lg bg-[#E9EBF8] border border-[#7697F4] border-[5px]',
+                'w-[250px] h-[325px] m-4',
+                'transform transition-transform duration-300 hover:scale-105'
             )}
         >
-            <div className="bg-blue-100 text-center p-4 rounded-t-lg">
-                <h2 className="text-xl font-semibold">STEP {stepNumber}</h2>
+            <div className="bg-blue-100 text-center p-4 rounded-t-[15px]">
+                <h2 className="text-[24px] font-semibold">STEP {stepNumber}</h2>
             </div>
             <div className="h-40 bg-gray-200 flex items-center justify-center">
                 {/* 이미지가 존재할 경우에만 렌더링 */}
@@ -31,8 +32,11 @@ const GuideCard: React.FC<GuideCardProps> = ({
                     />
                 )}
             </div>
-            <div className="bg-blue-100 text-center py-4 rounded-b-lg">
-                <p dangerouslySetInnerHTML={{ __html: footerText }} />
+            <div className="bg-blue-100 text-center py-4 rounded-b-[15px]">
+                <p
+                    className="text-[22px] text-[#575757] font-bold"
+                    dangerouslySetInnerHTML={{ __html: footerText }}
+                />
             </div>
         </div>
     );
