23922c5df774d1e1ee07e36a676374828018a67a
Yunseok <watemark1017@gmail.com>
2024-09-28 14:58:26 +0900

    [FEAT] - 이미지 모달과 멤버 카드 연결 + 모바일 약간 배려

--- src/components/Goalpage/MemberList/MemberCard.tsx
+++ src/components/Goalpage/MemberList/MemberCard.tsx
@@ -1,15 +1,46 @@
+import ShowImageModal from './ShowImageModal';
+import { useState } from 'react';
+import cn from '../../../lib/cn';
+
 interface MemberCardProps {
+    id: string; // 회원을 식별할 수 있는 고유 ID
     name: string;
 }
 
-export default function MemberCard({ name }: MemberCardProps) {
+export default function MemberCard({ id, name }: MemberCardProps) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
-        <div className="flex flex-col items-center justify-center w-[267px] h-[406px] bg-gray-200 rounded-lg">
-            <div className="w-[80px] h-[80px] bg-gray-300 rounded-full"></div>
-            <div className="text-lg font-bold">{name}</div>
-            <button className="w-[200px] h-[40px] bg-blue-400 rounded-lg">
+        <div className={cn(
+            "flex flex-col items-center justify-center bg-gray-200 rounded-lg relative p-6 md:p-20"
+            )}
+        >
+            <div className="w-16 md:w-32 h-16 md:h-32 bg-gray-300 rounded-full mb-4"></div>
+            <div className="text-base font-bold mb-4">{name}</div>
+            <button
+                className={cn(
+                    "w-16 md:w-32 h-8 md:h-12 bg-slate-400 text-white rounded-lg",
+                    "hover:bg-slate-700 transition text-sm md:text-xl font-semibold"
+                )}
+                onClick={handleOpenModal}
+            >
                 보러 가기
             </button>
+
+            {isModalOpen && 
+            <ShowImageModal 
+                memberId={id} 
+                memberName={name}
+                onClose={handleCloseModal} 
+            />}
         </div>
     );
 }
