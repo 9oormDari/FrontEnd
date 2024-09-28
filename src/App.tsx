8d9b9d5b3f553d3fc064e0cdb35a0a96195bffd6
liupei8979 <mintaekim@stamper.network>
2024-09-28 23:46:35 +0900

    [FEAT] - header 고정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
     return (
